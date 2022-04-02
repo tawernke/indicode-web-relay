d405404f56deba6b6f8d0268794da628e5acbc57
Tim Wernke <tim.wernke@enginedigital.com>
2022-04-02 17:41:02 -0400

    Added: base relay packages and next config

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    compiler: {
+      relay: {
+        // This should match relay.config.js
+        src: './',
+        artifactDirectory: './__generated__',
+        language: 'typescript',
+      },
+    },
+  }
