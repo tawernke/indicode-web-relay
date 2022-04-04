30c3649843a1374835235a25fc79cdcbd67e87ac
Tim Wernke <tim.wernke@enginedigital.com>
2022-04-04 07:31:11 -0400

    Changed: use relay query on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,66 +1,65 @@
 import { Button, Flex, Spinner, Text } from "@chakra-ui/react";
-import React from "react";
+import React, { Suspense } from "react";
+import {
+  useQueryLoader,
+  usePreloadedQuery,
+  graphql,
+  PreloadedQuery,
+  useLazyLoadQuery,
+} from "react-relay";
 import Card from "../components/Card";
 import { PageLayout } from "../components/PageLayout";
-import { usePublicProductsQuery } from "../generated/graphql";
+import { pagesPublicProductsQuery as PagesPublicProductsQueryType } from "../relay-types/pagesPublicProductsQuery.graphql";
 
-const Index = () => {
-  const { data, loading, fetchMore, variables } = usePublicProductsQuery({
-    variables: {
-      limit: 15,
-      cursor: null,
-    },
-    notifyOnNetworkStatusChange: true,
-  });
+const Query = graphql`
+  query pagesPublicProductsQuery {
+    products(isPublic: true, limit: 15, cursor: null) {
+      hasMore
+      publicProducts {
+        uuid
+        id
+        createdAt
+        updatedAt
+        name
+        price
+        quantity
+        imageUrl
+        isSold
+        isPublic
+        deleted
+      }
+    }
+  }
+`;
 
-  if (!loading && !data)
-    return (
-      <Text>No products to show at the moment, please check back later</Text>
-    );
+const Index: React.FC = () => {
+  // const [queryReference, loadQuery] = useQueryLoader<PagesPublicProductsQueryType>(
+  //   Query
+  // );
 
+  const data = useLazyLoadQuery<PagesPublicProductsQueryType>(Query, {});
   return (
     <PageLayout variant="full">
-      {!data && loading ? (
-        <Spinner
-          thickness="4px"
-          speed="0.65s"
-          emptyColor="gray.200"
-          color="blue.500"
-          size="xl"
-          position="absolute"
-          left="50%"
-          top="50%"
-        />
-      ) : (
+      <Suspense
+        fallback={
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+            position="absolute"
+            left="50%"
+            top="50%"
+          />
+        }
+      >
         <Flex mx={-2} pt={20} flexWrap="wrap" justifyContent="center">
           {data?.products.publicProducts.map((product) => {
             return <Card key={product.uuid} product={product} />;
           })}
         </Flex>
-      )}
-
-      {data && data.products.hasMore ? (
-        <Flex>
-          <Button
-            isLoading={loading}
-            m="auto"
-            my={10}
-            onClick={() => {
-              fetchMore({
-                variables: {
-                  limit: variables?.limit,
-                  cursor:
-                    data.products.publicProducts[
-                      data.products.publicProducts.length - 1
-                    ].createdAt,
-                },
-              });
-            }}
-          >
-            Load More
-          </Button>
-        </Flex>
-      ) : null}
+      </Suspense>
     </PageLayout>
   );
 };
