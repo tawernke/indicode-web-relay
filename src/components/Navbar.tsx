4581281d4e6f2dfd28e98d4f4efe1812312d4521
Tim Wernke <tim.wernke@enginedigital.com>
2022-04-04 07:32:19 -0400

    Changed: remove apollo queries from navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,20 +14,11 @@ import { useRouter } from "next/router";
 import React from "react";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import MenuIcon from "@material-ui/icons/Menu";
-import { useLogoutMutation, useMeQuery } from "../generated/graphql";
 import { isServer } from "../utils/isServer";
-import { useCartItems } from "../utils/useCartItems";
 
 export const NavBar: React.FC = () => {
   const router = useRouter();
-  const {
-    cartData: { cartCount },
-  } = useCartItems();
-  const [logout] = useLogoutMutation();
-  const { data, loading } = useMeQuery({
-    skip: isServer(),
-  });
-
+  
   return (
     <Flex zIndex={1} position="sticky" top={0} bg="#E6D8CB" py={2} px={4}>
       <Flex flex={1} m="auto" align="center" maxW={900}>
@@ -38,7 +29,7 @@ export const NavBar: React.FC = () => {
         </NextLink>
         <Box ml={"auto"}>
           <Flex>
-            {cartCount > 0 && (
+            {/* {cartCount > 0 && (
               <NextLink href="/cart">
                 <Link _hover={{ textDecoration: "none" }} mr={8}>
                   <ShoppingCartIcon />
@@ -47,34 +38,28 @@ export const NavBar: React.FC = () => {
                   </Box>
                 </Link>
               </NextLink>
-            )}
-            {!loading && data?.me && (
-              <Menu placement="bottom-end">
-                <MenuButton
-                  cursor="pointer"
-                  as={MenuIcon}
-                  aria-label="Options"
-                />
-                <MenuList>
-                  <MenuItem>
-                    <Box w={"100%"} onClick={() => router.push("/admin")}>
-                      Admin
-                    </Box>
-                  </MenuItem>
-                  <MenuItem>
-                    <Text
-                      w={"100%"}
-                      onClick={async () => {
-                        await logout();
-                        router.push("/");
-                      }}
-                    >
-                      Logout
-                    </Text>
-                  </MenuItem>
-                </MenuList>
-              </Menu>
-            )}
+            )} */}
+            <Menu placement="bottom-end">
+              <MenuButton cursor="pointer" as={MenuIcon} aria-label="Options" />
+              <MenuList>
+                <MenuItem>
+                  <Box w={"100%"} onClick={() => router.push("/admin")}>
+                    Admin
+                  </Box>
+                </MenuItem>
+                <MenuItem>
+                  <Text
+                    w={"100%"}
+                    onClick={async () => {
+                      await logout();
+                      router.push("/");
+                    }}
+                  >
+                    Logout
+                  </Text>
+                </MenuItem>
+              </MenuList>
+            </Menu>
           </Flex>
         </Box>
       </Flex>
