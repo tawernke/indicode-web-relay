fc5bbfd3e8f2f5d4f07e437c058f2b62a542594d
Tim Wernke <tim.wernke@enginedigital.com>
2022-04-04 07:31:44 -0400

    Changed: remove apollo provider and add relay provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,58 +1,18 @@
-import { ApolloProvider } from "@apollo/client";
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client/core";
 import { ChakraProvider } from "@chakra-ui/react";
-import { PaginatedPublicProducts } from "../generated/graphql";
+import { Suspense } from "react";
+import { RelayEnvironmentProvider } from "react-relay";
+import RelayEnvironment from "../RelayEnvironment";
 import theme from "../theme";
-import { CartItem } from "../utils/useCartItems";
-
-export const cartItemsVar = makeVar<CartItem[]>([]);
 
 function MyApp({ Component, pageProps }: any) {
-  const cache = new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          cartItems: {
-            read() {
-              return cartItemsVar();
-            },
-          },
-        },
-      },
-      Agenda: {
-        fields: {
-          publicProducts: {
-            keyArgs: [],
-            merge(
-              existing: PaginatedPublicProducts | undefined,
-              incoming: PaginatedPublicProducts
-            ): PaginatedPublicProducts {
-              return {
-                ...incoming,
-                publicProducts: [
-                  ...(existing?.publicProducts || []),
-                  ...incoming.publicProducts,
-                ],
-              };
-            },
-          },
-        },
-      },
-    },
-  });
-
-  const client = new ApolloClient({
-    uri: process.env.NEXT_PUBLIC_API_URL,
-    cache,
-    credentials: "include",
-  });
-
   return (
-    <ApolloProvider client={client}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </ApolloProvider>
+    <RelayEnvironmentProvider environment={RelayEnvironment}>
+      <Suspense fallback={"Loading..."}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </Suspense>
+    </RelayEnvironmentProvider>
   );
 }
 
